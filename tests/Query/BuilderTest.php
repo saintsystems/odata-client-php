28be91a90d317ef25038921c4ccf331636e2c4fc
Adam Anderly <adam@anderly.com>
2019-12-16 10:44:08 -0600

    Bump version constraints.

--- tests/Query/BuilderTest.php
+++ tests/Query/BuilderTest.php
@@ -493,4 +493,40 @@ class BuilderTest extends TestCase
         $this->assertEquals($expectedUri, $actualUri);
     }
 
+    public function testEntityMultipleWheres()
+    {
+        $builder = $this->getBuilder();
+
+        $entitySet = 'People';
+
+        $builder->from($entitySet)
+            ->where([
+                ['FirstName', 'Russell'],
+                ['LastName', 'Whyte'],
+            ]);
+
+        $expectedUri = 'People?$filter=(FirstName eq \'Russell\' and LastName eq \'Whyte\')';
+        $actualUri = $builder->toRequest();
+
+        $this->assertEquals($expectedUri, $actualUri);
+    }
+
+    public function testEntityMultipleWheresNested()
+    {
+        $builder = $this->getBuilder();
+
+        $entitySet = 'People';
+
+        $builder->from($entitySet)
+            ->where(function($query) {
+                $query->where('FirstName','Russell');
+                $query->where('LastName','Whyte');
+            });
+
+        $expectedUri = 'People?$filter=(FirstName eq \'Russell\' and LastName eq \'Whyte\')';
+        $actualUri = $builder->toRequest();
+
+        $this->assertEquals($expectedUri, $actualUri);
+    }
+
 }
