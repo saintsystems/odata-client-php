6a225986d3c3dff9770daf4d19c4ab5b6645f6dd
newton <newton@galileosoft.com.br>
2020-06-28 00:24:37 -0300

    Alterações para implementar os recursos criados por MasterStrap21: Added support for canonical functions for both OData 4 and previous versions.

--- src/Query/IGrammar.php
+++ src/Query/IGrammar.php
@@ -4,55 +4,78 @@ namespace SaintSystems\OData\Query;
 
 interface IGrammar
 {
-    /**
-     * Compile a select query into OData Uri
-     *
-     * @param Builder $query
-     *
-     * @return string
-     */
-    public function compileSelect(Builder $query);
+  /**
+   * Compile a select query into OData Uri
+   *
+   * @param Builder $query
+   *
+   * @return string
+   */
+  public function compileSelect(Builder $query);
 
-    /**
-     * Get the grammar specific operators.
-     *
-     * @return array
-     */
-    public function getOperators();
+  /**
+   * Get the grammar specific operators.
+   *
+   * @return array
+   */
+  public function getOperators();
 
-    /**
-     * Get the appropriate query parameter place-holder for a value.
-     *
-     * @param mixed $value
-     *
-     * @return string
-     */
-    public function parameter($value);
+  /**
+   * Get the grammar specific functions.
+   *
+   * @return array
+   */
+  public function getFunctions();
 
-    /**
-     * Determine if the given value is a raw expression.
-     *
-     * @param mixed $value
-     *
-     * @return bool
-     */
-    public function isExpression($value);
+  /**
+   * Get the grammar specific operators and functions.
+   *
+   * @return array
+   */
+  public function getOperatorsAndFunctions();
 
-    /**
-     * Get the value of a raw expression.
-     *
-     * @param \Illuminate\Database\Query\Expression $expression
-     *
-     * @return string
-     */
-    public function getValue($expression);
+  /**
+   * Prepare the appropriate URI value for a where value.
+   *
+   * @param mixed $value
+   *
+   * @return string
+   */
+  public function prepareValue($value);
 
-    /**
-     * Convert an array of property names into a delimited string.
-     *
-     * @param array $properties
-     *
-     * @return string
-     */
-    public function columnize(array $properties);
+  /**
+   * Get the appropriate query parameter place-holder for a value.
+   *
+   * @param mixed $value
+   *
+   * @return string
+   */
+  public function parameter($value);
+
+  /**
+   * Determine if the given value is a raw expression.
+   *
+   * @param mixed $value
+   *
+   * @return bool
+   */
+  public function isExpression($value);
+
+  /**
+   * Get the value of a raw expression.
+   *
+   * @param \Illuminate\Database\Query\Expression $expression
+   *
+   * @return string
+   */
+  public function getValue($expression);
+
+  /**
+   * Convert an array of property names into a delimited string.
+   *
+   * @param array $properties
+   *
+   * @return string
+   */
+  public function columnize(array $properties);
 }
