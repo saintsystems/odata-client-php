234018f4227291d2da248c3e2e34c95d616361d5
motze92 <41009610+motze92@users.noreply.github.com>
2021-11-25 18:22:38 +0100

    Fix: Delete works only for entity
    since delete only worked on a single entity I added whereKey($id)

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -892,8 +892,10 @@ class Builder
      *
      * @return boolean
      */
-    public function delete($options = null)
+    public function delete($id, $options = null)
     {
+        $this->whereKey($id);
+        
         $results = $this->processor->processSelect($this, $this->runDelete());
 
         return true;
